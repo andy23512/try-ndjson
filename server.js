e977791e0213d926e30a6e50d04c4b012d4245cc
andy23512 <andy23512@gmail.com>
2018-04-01 10:07:58 +0800

    use ndjson module to parse ndjson file, output each chunk every 500 milliseconds

--- server.js
+++ server.js
@@ -2,14 +2,29 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
+const ndjson = require('ndjson');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/api', (req, res) => {
-  let readStream = fs.createReadStream(__dirname + '/todos.ndjson');
-  res.writeHead(200, { 'Content-Type': 'application/ndjson' });
-  readStream.on('open', () => {
-    readStream.pipe(res); // pipe stream to response object
+app.get('/', (req, res) => {
+  let readStream = fs
+    .createReadStream(__dirname + '/todos.ndjson')
+    .pipe(ndjson.parse());
+
+  const chunks = [];
+  readStream.on('data', data => {
+    chunks.push(JSON.stringify(data));
+  });
+
+  readStream.on('end', () => {
+    let id = setInterval(() => {
+      if (chunks.length) {
+        res.write(chunks.shift() + '\n');
+      } else {
+        clearInterval(id);
+        res.end();
+      }
+    }, 500);
   });
 });
 
