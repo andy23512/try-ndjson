877107c94547087ac6506244742f6893243d8275
andy23512 <andy23512@gmail.com>
2018-04-01 10:02:16 +0800

    serve ndjson via readstream

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/api', (req, res) => {
+  let readStream = fs.createReadStream(__dirname + '/todos.ndjson');
+  res.writeHead(200, { 'Content-Type': 'application/ndjson' });
+  readStream.on('open', () => {
+    readStream.pipe(res); // pipe stream to response object
+  });
 });
 
 app.listen(3000, () => {
